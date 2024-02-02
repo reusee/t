8ce5d3c6e3f8abf941cbf1d9a0ef3eb8d96b0c8f
reus <reusee@gmail.com>
2024-02-02 13:44:20 +0800

    implement

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/reusee/t
+
+go 1.22
+
+require github.com/reusee/e5 v0.0.0-20230610121337-9deb1a7b70ae
---
+++ main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"log/slog"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+)
+
+func main() {
+	var llamaCPPPath string
+	for _, cmd := range []string{
+		"llama.cpp",
+		"llama",
+	} {
+		path, err := exec.LookPath(cmd)
+		if err == nil {
+			llamaCPPPath = path
+			break
+		}
+	}
+	if llamaCPPPath == "" {
+		slog.Warn("llama.cpp not found")
+		return
+	}
+
+	homeDir, err := os.UserHomeDir()
+	ce(err)
+	cacheDir, err := os.UserCacheDir()
+	ce(err)
+
+	var modelPath string
+	for _, path := range []string{
+		filepath.Join(homeDir, ".llama.model"),
+		filepath.Join(homeDir, "llama.model"),
+	} {
+		_, err := os.Stat(path)
+		if err == nil {
+			modelPath = path
+			break
+		}
+	}
+	if modelPath == "" {
+		slog.Warn("no model")
+		return
+	}
+
+	text := strings.Join(os.Args[1:], " ")
+
+	cmd := exec.Command(
+		llamaCPPPath,
+		"--model", modelPath,
+		"--prompt", `Please translate [`+text+`] to English in simple and concise words`,
+		"--prompt-cache", filepath.Join(cacheDir, "llm-translate-cache"),
+		"--ctx-size", "0",
+		"--color",
+		"--mlock",
+		"--log-disable",
+	)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	ce(cmd.Run())
+}
